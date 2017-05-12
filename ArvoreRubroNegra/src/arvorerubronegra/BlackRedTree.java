d415da639d27f046bc9bec9ebacf9b9e04bca28c
Joao Paulo Silva de Souza <joao.paulo.silvasouza@hotmail.com>
2017-05-12 06:16:32 -0300

    Visualização Rubro-Negra

--- ArvoreRubroNegra/src/arvorerubronegra/BlackRedTree.java
+++ ArvoreRubroNegra/src/arvorerubronegra/BlackRedTree.java
@@ -1,7 +1,17 @@
 package arvorerubronegra;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BlackRedTree extends BinarySearchTree {
     public static Node root;
+    public static Map<Integer, List<Node>> treeLevels;
     
     public BlackRedTree() {
         this.root = null;
@@ -147,6 +157,54 @@ public class BlackRedTree extends BinarySearchTree {
             displayTree(root.left);
             System.out.print(" " + root.data + "(" + (root.color == 0 ? "p" : "v") + ")");
             displayTree(root.right);
+        }
+    }
+    
+    public void generateTreeView (Node root) {
+        treeLevels = new HashMap<>();
+        buildTreeLevels(root, 0);
+        String htmlOutput = "<!doctype html> <html> <head> <script type='text/javascript' src='jquery-1.7.2.min.js'></script><script type='text/javascript' src='jqSimpleConnect.js'></script> <meta charset='utf-8'> <title>Visualização da árvore Rubro-Negra</title> <meta name='description' content='Visualização da árvore Rubro-Negra'> <meta name='author' content='20151014040004'> <style> .row { width: 100%; text-align: center; } .node { width: 30px; border-radius: 6px; border: 1px solid black; display: inline-table; margin-top: 10px; margin-bottom: 10px; } .redNode { background-color: red;  color: black; } .blackNode { background-color: black; color: white; } </style> </head> <body>";
+        htmlOutput += "<div class='row'><div class='node blackNode' id='_" + root.data + "'>" + root.data + "</div></div>";
+        int i = 1;
+        int startMagin = 80;
+        while (treeLevels.get(i) != null) {
+            htmlOutput += "<div class='row'>";
+            int k = 0;
+            for (int j = 0; j < (Math.pow(2, i)); j++) {
+                int margin = new Double((startMagin/i)*1.2).intValue();
+                if (k < treeLevels.get(i).size() && ((Node)treeLevels.get(i).get(k)).parent.equals((Node)treeLevels.get(i-1).get(j/2))) {
+                    htmlOutput += String.format("<div id='%s' style='margin-left:%dpx; margin-right: %dpx;' class='node %s'>%d</div>", String.format("%d_%d", ((Node)treeLevels.get(i-1).get(j/2)).data, ((Node)treeLevels.get(i).get(k)).data), margin, margin,(((Node)treeLevels.get(i).get(k)).color == 0 ? "blackNode" : "redNode"), ((Node)treeLevels.get(i).get(k)).data);
+                    k++;
+                } else {
+                    htmlOutput += String.format("<div style='margin-left:%dpx; margin-right: %dpx;' class='node'>-</div>", (startMagin/i),(startMagin/i));
+                }
+            }
+            htmlOutput += "</div>";
+            i++;
+        }
+        htmlOutput += "<script>$(\".node[id]\").each(function(q,b){ var children = $(\".node[id^=\" + b.id.split(\"_\")[1] + \"]\"); var i = 0; for (;children[i];) { jqSimpleConnect.connect(\"#\" + b.id, \"#\" + children[i].id, {radius: 2, color: 'black', anchorA: \"vertical\", anchorB: \"vertical\"}); i++; } b.style.zIndex = \"999\"; b.style.position = \"relative\"; }); window.onresize = function () { jqSimpleConnect.repaintAll(); };</script>";
+        htmlOutput += "</body></html>";
+        saveToFile(htmlOutput);
+    }
+    
+    protected void buildTreeLevels(Node root, int level) {
+        if (root != null) {
+            buildTreeLevels (root.left, level+1);
+            if (treeLevels.get(level) == null) {
+                treeLevels.put(level, new ArrayList<>());
+            }
+            treeLevels.get(level).add(root);
+            buildTreeLevels (root.right, level+1);
+        }
+    }
+    
+    protected void saveToFile(String fileContents) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+              new FileOutputStream("avlTree.html"), "utf-8"))) {
+            writer.write(fileContents);
+            writer.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
     
